f7713b5e870dd15637c5403889247ba21b034f59
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-05 15:54:02 +0200

    refactored

--- src/models/Elev.ts
+++ src/models/Elev.ts
@@ -1,22 +1,27 @@
 
+
+export interface Time {
+  secondsTotal: number,
+    
+  minutesPartial: number,
+  hoursPartial: number,
+}
+
+export interface FlexTime extends Time {
+  isNegative: boolean,
+}
+
+
+export interface Vacation {
+  used: number,
+  planned: number,
+  left: number,
+}
+
 export interface Elev {
   id: number,
   name: string,
   department: string,
   flex: FlexTime,
   vacation: Vacation,
-}
-
-export interface FlexTime {
-  secondsTotal: number,
-  isNegative: boolean,
-  
-  minutesPartial: number,
-  hoursPartial: number,
-}
-
-export interface Vacation {
-  used: number,
-  planned: number,
-  left: number,
 }
