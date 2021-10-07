ba662dccee9509d32e700ade91eca8456973d989
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-07 15:31:32 +0200

    remade db architecture

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,24 +1,7 @@
 import { Employee } from "../models/Employee";
 
-export enum OperationStatus {
-  Ok,
-  Empty,
-  BadRequest,
-  DbError,
-  ServerError
-}
-
-export interface SingleOperationResult <Data>{
-  status: OperationStatus,
-  data?: Data,
-  error?: Error,
-  catched?: unknown,
-}
-
 export abstract class Database {
 
-  public abstract isElevIdValid: (elev: Employee) => Promise<SingleOperationResult<boolean>>;
-  public abstract addElev: (elev: Employee) => Promise<SingleOperationResult<Employee>>;
-  public abstract getElev: (id: number) => Promise<SingleOperationResult<Employee>>;
+  public abstract getEmployeeById: (id: number) => Promise<Employee | null>; 
 
 }
