458478b6ac1903bc9d0f076036d985c2470f47ec
Simon From Jakobsen <sfja.skp@edu.mercantec.dk>
2021-10-08 11:53:59 +0200

    expanded Database

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,7 +1,12 @@
 import { Employee } from "../models/Employee";
+import { Team } from "../models/Team";
 
 export abstract class Database {
 
-  public abstract getEmployeeById: (id: number) => Promise<Employee | null>; 
+  public abstract getEmployeeById: (id: number) => Promise<Employee | null>;
+  public abstract replaceEmployeeById: (replace: Employee, upsert: boolean) => Promise<Employee | null>;
+
+  public abstract getTeamById: (id: number) => Promise<Team | null>;
+  public abstract replaceTeamById: (replace: Team, upsert: boolean) => Promise<Team | null>;
 
 }
