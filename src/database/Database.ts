1fa484494c76a548e2584788b7a9e9ae14f157e4
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-20 17:04:06 +0200

    funcs to methods

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,10 +1,17 @@
-import { CheckTask } from "../models/CheckTask";
+import { LoggedError } from "../models/LoggedError";
+import { Task } from "../models/Task";
 
 export abstract class Database {
 
-  public abstract getUniqueTaskId: () => Promise<number>;
+  public abstract getUniqueTaskId(): Promise<number>;
+  public abstract getUniqueErrorId(): Promise<number>
   
-  public abstract getCheckTaskCount: () => Promise<number>;
-  public abstract insertCheckTask: (task: CheckTask) => Promise<CheckTask>;
+  public abstract getTaskCount(): Promise<number>;
+  public abstract getTasks(): Promise<Task[]>;
+  public abstract insertTask(task: Task): Promise<Task>;
+
+  public abstract getErrorCount(): Promise<number>;
+  public abstract getErrors(): Promise<LoggedError[]>;
+  public abstract insertError(error: LoggedError): Promise<LoggedError>;
 
 }
