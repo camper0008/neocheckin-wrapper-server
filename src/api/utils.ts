65171e30aa30565d64b0d6a9e521c2fb85763543
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-22 13:30:43 +0200

    added instrukdb to handle

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from "express";
 import { Database } from "../database/Database";
+import { Instrukdb } from "../instrukdb/Instrukdb";
 
 export interface Respondable {
   data?: any,
@@ -7,6 +8,6 @@ export interface Respondable {
 }
 
 // TODO determine behavor of `type N<T = K> = I<T>; N<any> and N<undefined>;` what is T in I<T>
-export type Handle<ReqBody = any, ResBody = Respondable> = (db: Database) => 
+export type Handle<ReqBody = any, ResBody = Respondable> = (db: Database, idb: Instrukdb.API) => 
   (req: Request<undefined, ResBody, ReqBody>, res: Response<ResBody>) => Promise<any>;
 
