40334dc6b4d5cb2e13d8519f6476b9cd6d448e43
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-22 13:30:22 +0200

    added rfid

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -1,10 +1,12 @@
 import { LoggedError } from "../models/LoggedError";
+import { Rfid } from "../models/Rfid";
 import { Task } from "../models/Task";
 import { TaskType } from "../models/TaskType";
 
 export abstract class Database {
 
   public abstract getUniqueTaskId(): Promise<number>;
+  public abstract getUniqueRfidId(): Promise<number>;
   public abstract getUniqueErrorId(): Promise<number>
   
   public abstract getTaskCount(): Promise<number>;
@@ -14,6 +16,10 @@ export abstract class Database {
   public abstract getTaskTypes(): Promise<TaskType[]>;
   public abstract getTaskType(id: number): Promise<TaskType>;
   public abstract replaceTaskTypes(taskTypes: TaskType[]): Promise<number>;
+
+  public abstract getRfidByEmployeeId(employeeId: number): Promise<Rfid>;
+  public abstract getRfidByRfid(rfid: string): Promise<Rfid>;
+  public abstract insertRfid(rfid: Rfid): Promise<Rfid>;
 
   public abstract getErrorCount(): Promise<number>;
   public abstract getErrors(): Promise<LoggedError[]>;
