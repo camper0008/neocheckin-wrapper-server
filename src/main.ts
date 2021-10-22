b45d5e36c855aab8fef237ca0c8e482ad1d671d4
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-22 13:28:49 +0200

    added top level instrukdb connection

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { config } from 'dotenv';
 import { api } from './api/api';
 import { MemoryDB } from './database/MemoryDB';
+import { MockInstrukdb } from './instrukdb/MockInstrukdb';
 
 
 const main = async () => {
   config();
   const db = new MemoryDB();
-  await api(db);
+  const idb = new MockInstrukdb();
+  await api(db, idb);
 }
 
 main().catch(error => console.error(error));
