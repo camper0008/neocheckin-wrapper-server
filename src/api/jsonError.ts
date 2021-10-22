14d7cc8cddba02d06cda2acabc9fe060bc7c4f24
Simon From <simonfromjakobsen@gmail.com>
2021-10-22 02:16:12 +0200

    console erroring

--- src/api/jsonError.ts
+++ src/api/jsonError.ts
@@ -3,6 +3,8 @@ import { ErrorRequestHandler } from "express"
 export const jsonError = () => {
 
   const jsonErrorMiddleware: ErrorRequestHandler =  async (err, req, res, next) => {
+    if (err && err.type && err.type === 'entity.parse.failed')
+      console.error(`${new Date().toISOString()} [API]: Recieved request with invalid JSON request body`);
     res.status(400).json({error: err})
   }
 
