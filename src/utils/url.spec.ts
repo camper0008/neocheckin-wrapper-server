c346e91b072eef6d0a79d68ae4d2c9e4523aa730
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-26 15:41:53 +0200

    paramstring

--- src/utils/url.spec.ts
+++ src/utils/url.spec.ts
@@ -1,9 +1,26 @@
-import { insureUrlPathEnd } from './url';
+import { insureUrlPathEnd, paramString } from './url';
 
-it('should add / to the end', () => {
-  expect(insureUrlPathEnd('https://instrukdb/api')).toBe('https://instrukdb/api/');
+describe('insureUrlPathEnd', () => {
+
+  it('should add / to the end', () => {
+    expect(insureUrlPathEnd('https://instrukdb/api')).toBe('https://instrukdb/api/');
+  });
+  
+  it('should add not / to the end', () => {
+    expect(insureUrlPathEnd('https://instrukdb/api/')).toBe('https://instrukdb/api/');
+  });
+
 });
 
-it('should add not / to the end', () => {
-  expect(insureUrlPathEnd('https://instrukdb/api/')).toBe('https://instrukdb/api/');
+describe('paramString', () => {
+
+  it('should return empty', () => {
+    expect(paramString({})).toBe('');
+  });
+
+  it('should return data as param string', () => {
+    const data = {name: 'foo'};
+    expect(paramString(data)).toBe('?name=foo');
+  });
+
 });
