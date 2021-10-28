8299a4c193e276cd8f9c341bf4e79b81d382adda
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-28 12:31:48 +0200

    api token

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,21 @@
 import cors from 'cors';
-import express, { json, Router, urlencoded } from 'express';
+import express, { json, NextFunction, Request, Response, Router, urlencoded } from 'express';
 import { join } from 'path';
 import { Database } from '../database/Database';
 import { Instrukdb } from '../instrukdb/Instrukdb';
+import { getApiToken } from '../utils/env';
 import { employeesRoutes } from './employees';
 import { jsonError } from './jsonError';
 import { serveHttp, serveHttps } from './servers';
 import { tasksRoutes } from './tasks';
+
+export const apiTokenAuth = (apiToken: string) => async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers['token'] as string;
+  if (token === apiToken)
+    next();
+  else
+    res.status(400).json({error: 'access denied'})
+}
 
 export const api = async (db: Database, idb: Instrukdb.API) => {
   const app = express();
@@ -14,6 +23,7 @@ export const api = async (db: Database, idb: Instrukdb.API) => {
   app.use(cors());
   app.use(json());
   app.use(urlencoded({extended: true}));
+  app.use(apiTokenAuth(getApiToken()));
   app.use(jsonError());
 
   // TODO implement error state
