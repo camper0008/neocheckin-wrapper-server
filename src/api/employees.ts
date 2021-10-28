36bc8f3a7e04e00a18d99d487e2338e9046138ed
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-10-28 11:47:17 +0200

    minor fixes to match spec

--- src/api/employees.ts
+++ src/api/employees.ts
@@ -9,13 +9,13 @@ import { Handle, Respondable } from "./utils";
 
 
 export interface GetEmployeesAllRes extends Respondable {
-  data?: (Employee & {photo: string})[],
+  data?: (Omit<Employee, 'rfid'> & {photo: string, rfid: string})[],
 }
 
 export const getEmployeesAllHandle: Handle<any, GetEmployeesAllRes> = (db: Database, idb: Instrukdb.API) =>
 async (req, res) => {
   try {
-    res.status(200).json({data: await getAllEmployees(db, idb)});
+    res.status(200).json({data: (await getAllEmployees(db, idb)).map(e => ({...e, rfid: e.rfid.toString()}))});
   } catch (catched) {
     res.status(500).json({error: 'server error'});
     console.error(catched);
