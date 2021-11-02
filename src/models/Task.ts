e33c74e49b574373130854387f9b01280cddaa02
Simon From Jakobsen <simonfromjakobsen@gmail.com>
2021-11-02 15:28:41 +0100

    renamed taskId and added enum

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,10 +1,18 @@
 import { Identifiable } from "../database/Identifyable";
 
+export enum TaskStatus {
+  WAITING     = 'waiting',
+  PROCESSING  = 'processing',
+  SUCCEEDED   = 'succeeded',
+  FAILED      = 'failed',
+}
+
 export interface Task extends Identifiable {
   name: string,
-  taskId: number,
+  taskTypeId: number,
   date: Date,
   systemIdentifier: string,
   employeeRfid: string,
   highLevelApiKey: string,
+  status: TaskStatus,
 }
