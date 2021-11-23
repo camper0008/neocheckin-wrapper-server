3d48d810c1bf417ea15fb911142d7030a5bd219b
piet <spetakel123@gmail.com>
2021-11-23 15:56:21 +0100

    use test logger

--- src/tasks/addTasks.ts
+++ src/tasks/addTasks.ts
@@ -1,4 +1,8 @@
+import { tasksRoutes } from "../api/tasks";
 import { Database } from "../database/Database";
+import { formatTask } from "../logs/formatLogs";
+import { Logger } from "../logs/Logger";
+import { LogItem, LogStatus } from "../logs/LogItem";
 import { Task, TaskStatus } from "../models/Task";
 
 export interface AddTaskRequest {
@@ -12,15 +16,17 @@ export interface AddTaskRequest {
 }
 
 export const getDateFromDateOrString = (date?: Date | string) => {
-  if (date instanceof Date) return date;
-  if (typeof date === 'string') return new Date(date);
-  return new Date();
+  if (date instanceof Date) 
+    return date;
+  else 
+    if (typeof date === 'string') return new Date(date);
+  else 
+    return new Date();
 }
 
-export const addTask = async (task: AddTaskRequest, db: Database): Promise<Task> => {
-  const {name, date} = task;
-  if (name === '')
-    throw new Error('name empty');
+export const addTask = async (task: AddTaskRequest, db: Database, logger?: Logger): Promise<Task> => {
+  const {date} = task;
+  checkTaskName(task, logger);
   const id = await db.getUniqueTaskId();
   const insert = await db.insertTask({
     ...task,
@@ -29,5 +35,18 @@ export const addTask = async (task: AddTaskRequest, db: Database): Promise<Task>
     status: TaskStatus.WAITING,
     taskTypeId: task.taskTypeId
   });
+  logger?.logAddTask(task, LogStatus.success);
   return insert;
 }
+
+const invalidTaskName = (name: string) => {
+  return name === ''
+}
+
+function checkTaskName(task: AddTaskRequest, logger: Logger | undefined) {
+  if (invalidTaskName(task.name)) {
+    logger?.logAddTask(task, LogStatus.error);
+    throw new Error('name empty');
+  }
+}
+
