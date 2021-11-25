817158fbdc787186e9426aa0b22e9220842b1aa2
Simon From Jakobsen <sfja.skp@edu.mercantec.dk>
2021-11-25 11:10:39 +0100

    fix/dat_format

--- src/utils/timedate.spec.ts
+++ src/utils/timedate.spec.ts
@@ -2,8 +2,8 @@ import { formatFileFriendly, getDateFromDateOrString } from "./timedate";
 
 describe('formatFileFriendly', () => {
   it('should format time and date', () => {
-    const date = new Date('2021-11-23T14:48:19.796Z')
-    expect(formatFileFriendly(date)).toBe(`2021-11-23_14-48-19`)
+    const date = new Date('2021-11-23T14:48:19.796Z');
+    expect(formatFileFriendly(date)).toBe(`2021-11-23_14-48-19`);
   });
 });
 
