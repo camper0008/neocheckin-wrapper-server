1cf941ca775872663ad1110519d361fb26b416f6
Simon From Jakobsen <sfja.skp@edu.mercantec.dk>
2021-11-29 14:21:08 +0100

    updated ssl script

--- certificates/generate_ssl.sh
+++ certificates/generate_ssl.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash
 
 # This script generates local temporary SSL key and certificate
 
 # Run this script inside the certificates folder
 
-openssl req -x509 -newkey rsa:2048 -keyout keytmp.pem -out cert.pem -days 365
-
-openssl rsa -in keytmp.pem -out key.pem
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout key.pem -out cert.pem \
+  -subj "/C=US/ST=Chicago/L=Geleen/O=Bruh bruh/OU=Development/CN=localhost/emailAddress=bruh@yahoo.com"
